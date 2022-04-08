1a442b0a7ab5defbecec5bde1ddf551a441506b3
PaolaP <paolaperez8@gmail.com>
2022-04-08 04:43:59 +0000

    exercise 5 calculations

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,25 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+#Order.sum("subtotal")
+
+
+total_revenue = Store.sum("annual_revenue")
+
+puts "Total Revenue for stores #{total_revenue}"
+
+
+average_annual_revenue = Store.average("annual_revenue")
+
+puts "Average Annual Revenue for stores #{average_annual_revenue}"
+
+#Book.where("title = ?", params[:title])
+
+
+
+wealthy_stores = Store.where(total_revenue >= 1000000)
+#puts wealthy_stores.count
+# needs fix
+
+puts "The wealthy stores are #{wealthy_stores}"
