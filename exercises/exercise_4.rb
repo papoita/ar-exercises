7487311d06bf7104bef0c561a814cdcb163e660c
PaolaP <paolaperez8@gmail.com>
2022-04-08 04:03:33 +0000

    exercise 4 where and conditionals

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,26 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+store = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+
+store = Store.create(name: "Whistler",annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+
+store = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+puts Store.count
+
+@mens_stores = Store.where(mens_apparel: true)
+
+
+@mens_stores.each do |store|
+  puts "Store #{store.name} produces annual revenue of #{store.annual_revenue}"
+
+end
+
+@womens_stores = Store.where(womens_apparel: true)
+
+@womens_stores.each do |store|
+  if store[:annual_revenue] >= 1000000
+    puts "This women's only store #{store.name} produces annual revenue of #{store.annual_revenue}"
+  end
+end
