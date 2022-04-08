bc6f52c467dd69f32e7e32a6dcf21f2f08c310ea
PaolaP <paolaperez8@gmail.com>
2022-04-08 02:53:23 +0000

    exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,16 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+#example
+# user = User.find_by(name: 'David')
+# user.name = 'Dave'
+# user.save
+
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+@store1.name = "EcoStore"
+@store1.save
+
+pp @store1
+pp @store2
